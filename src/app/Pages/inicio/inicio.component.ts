928b00e507b5b34cf35f3597b4b29ab4f466c19d
guillermoGodoyPro <guillermo.rgo92@gmail.com>
2025-05-27 15:34:47 -0300

    parte del front

---
+++ src/app/Pages/inicio/inicio.component.ts
@@ -0,0 +1,65 @@
+import { Component, inject } from '@angular/core';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { EmpleadoService } from '@app/Services/empleado.service';
+import { Empleado } from '@app/Models/Empleado';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-inicio',
+  imports: [
+    MatCardModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  templateUrl: './inicio.component.html',
+  styleUrl: './inicio.component.css'
+})
+export class InicioComponent {
+
+  private empleadoServicio = inject(EmpleadoService)
+  public listaEmpleados: Empleado[] = [];
+  public displayedColumns: string[] = ['nombreCompleto', 'correo', 'sueldo', 'fechaContrato','accion'];
+
+  obtenerEmpleados() {
+    this.empleadoServicio.lista().subscribe({
+      next: (data) => {
+        if (data.length > 0) {
+          this.listaEmpleados = data;
+        }
+      },
+      error: (e) => { console.log(e.message) }
+    })
+  }
+
+  constructor(private router: Router) { 
+    this.obtenerEmpleados();
+  }
+
+  nuevo() {
+    this.router.navigate(['/empleado', 0])
+  }
+
+  editar(objeto: Empleado) {
+    this.router.navigate(['/empleado', objeto.idEmpleado])
+  }
+
+  eliminar(objeto: Empleado) {
+    if (confirm("Desea eliminar el empleado" + objeto.nombreCompleto)) {
+      this.empleadoServicio.eliminar(objeto.idEmpleado).subscribe({
+        next: (data) => {
+          if (data.isSuccess) {
+            this.obtenerEmpleados();
+          }else{
+            alert("no se pudo eliminar")
+          }
+        },
+        error: (e) => { console.log(e.message) }
+      })
+    }
+  }
+}
---
+++ src/app/Services/empleado.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Empleado } from '@app/Models/Empleado';
+import { ResponseAPI } from '@app/Models/ResponseAPI';
+import { appsettings } from '@app/Settings/appsettings';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmpleadoService {
+
+  private http = inject(HttpClient);
+  private apiUrl: string = appsettings.apiUrl + "Empleado"
+
+  constructor() { }
+
+  lista() {
+    return this.http.get<Empleado[]>(this.apiUrl);
+  }
+
+  obtener(id: number) {
+    return this.http.get<Empleado[]>(`${this.apiUrl}/${id}`);
+  }
+
+  crear(objeto: Empleado) {
+    return this.http.post<ResponseAPI>(this.apiUrl, objeto);
+  }
+
+  editar(objeto: Empleado) {
+    return this.http.put<ResponseAPI>(this.apiUrl, objeto);
+  }
+
+  eliminar(id: number) {
+    return this.http.delete<ResponseAPI>(`${this.apiUrl}/${id}`);
+  }
+}
